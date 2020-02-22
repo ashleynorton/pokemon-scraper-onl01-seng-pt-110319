da67c49bcd28ccc08549d6f653c3ce70d3b41248
Ashley Norton <nortonashleym@gmail.com>
2020-02-22 04:58:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,9 +11,12 @@ class Pokemon
   
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
-    
   end 
   
-  
+  def self.find(id, db)
+    #find pokemon by id 
+    #return a new pokemon object
+    db.execute("SELECT * FROM pokemon WHERE id = ?")
+  end 
   
 end
