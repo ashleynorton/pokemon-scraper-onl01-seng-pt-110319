eefdd42bcba3fab80a6dfc9cd9ae85301148ba53
Ashley Norton <nortonashleym@gmail.com>
2020-02-22 04:49:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
   end 
   
   def self.save(name, type, db)
-    db.execute
+    db.execute(INSERT INTO pokemon VALU"ES (?, ?);)
     
     DB[:conn].execute(sql, self.name, self.type, self.db)
     
